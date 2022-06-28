31e1584aa90cede72dcd1aca5d3b03ac19b5bea7
Salman Shahzad <salshahzad@outlook.com>
2022-06-27 20:21:52 -0400

    Add Chakra UI

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,11 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
 
 const root = createRoot(document.getElementById("root")!);
-root.render(<App />);
+root.render(
+  <ChakraProvider>
+    <App />
+  </ChakraProvider>
+);
