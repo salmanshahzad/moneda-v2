4387b6926b283f8b7902ea62689eb17df122e791
Salman Shahzad <salshahzad@outlook.com>
2022-06-27 20:27:42 -0400

    Add React Router

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,16 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import App from "./App";
 
 const root = createRoot(document.getElementById("root")!);
 root.render(
   <ChakraProvider>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </ChakraProvider>
 );
