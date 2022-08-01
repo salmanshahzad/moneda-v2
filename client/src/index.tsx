4942c8580cd8583e95d10cbea8ca3afefd2d4a05
Salman Shahzad <salshahzad@outlook.com>
2022-08-01 10:00:43 -0400

    Add notification on add transaction

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,4 +1,5 @@
 import { MantineProvider } from "@mantine/core";
+import { NotificationsProvider } from "@mantine/notifications";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
@@ -10,18 +11,20 @@ import SignOut from "./pages/SignOut";
 
 const root = createRoot(document.getElementById("root")!);
 root.render(
-  <MantineProvider withNormalizeCSS>
-    <BrowserRouter>
-      <Routes>
-        <Route element={<AuthGuard />}>
-          <Route path="/" element={<Home />} />
-          <Route element={<Layout />}>
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/signout" element={<SignOut />} />
+  <MantineProvider withGlobalStyles withNormalizeCSS>
+    <NotificationsProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AuthGuard />}>
+            <Route path="/" element={<Home />} />
+            <Route element={<Layout />}>
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/signout" element={<SignOut />} />
+            </Route>
           </Route>
-        </Route>
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </BrowserRouter>
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </BrowserRouter>
+    </NotificationsProvider>
   </MantineProvider>
 );
--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,6 +1,8 @@
 import { Grid } from "@mantine/core";
 import { useMediaQuery } from "@mantine/hooks";
+import { showNotification } from "@mantine/notifications";
 import { useState } from "react";
+import { Check } from "tabler-icons-react";
 
 import api from "../api";
 import AddTransaction, {
@@ -11,12 +13,21 @@ import useStore from "../store";
 function Dashboard(): JSX.Element {
   const span = useMediaQuery("(min-width: 900px)") ? 6 : 12;
   const user = useStore((state) => state.user)!;
-  const [addTransactionErrors, setAddTransactionErrors] = useState<Partial<Record<keyof AddTransactionFields, string>>>({});
+  const [addTransactionErrors, setAddTransactionErrors] = useState<
+    Partial<Record<keyof AddTransactionFields, string>>
+  >({});
 
-  async function addTransaction(values: AddTransactionFields): Promise<boolean> {
+  async function addTransaction(
+    values: AddTransactionFields
+  ): Promise<boolean> {
     setAddTransactionErrors({});
     const { data, status } = await api.post("transaction", values);
     if (status === 201) {
+      showNotification({
+        color: "green",
+        icon: <Check />,
+        message: "Transaction added",
+      });
       return true;
     } else if (status === 422) {
       setAddTransactionErrors(api.extractErrorMessages(data));
