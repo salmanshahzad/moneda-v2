341624a4186b8537a55ff17778bf886ec56caed4
Salman Shahzad <salshahzad@outlook.com>
2022-08-01 20:11:39 -0400

    Modify Block to allow single row

--- client/src/components/ui/Block.tsx
+++ client/src/components/ui/Block.tsx
@@ -1,15 +1,28 @@
-import { Paper, Text } from "@mantine/core";
+import { Group, Paper, Text } from "@mantine/core";
 
 export interface BlockProps {
   children: JSX.Element;
+  singleRow?: boolean;
   title: string;
 }
 
 function Block(props: BlockProps): JSX.Element {
   return (
     <Paper p="md" shadow="md">
-      <Text weight="bold">{props.title}</Text>
-      {props.children}
+      {props.singleRow && (
+        <Group position="apart">
+          <Text weight="bold">{props.title}</Text>
+          {props.children}
+        </Group>
+      )}
+      {!props.singleRow && (
+        <>
+          <Text mb="xs" weight="bold">
+            {props.title}
+          </Text>
+          {props.children}
+        </>
+      )}
     </Paper>
   );
 }
